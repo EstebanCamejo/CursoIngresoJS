7ee99e57cf870486e1f920cf35382bad1d0ea0c5
Your Name <you@example.com>
2022-02-02 20:01:15 -0300

    cambio dia 2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Camejo OK");
+	alert("tomar cafe hace bien ");
 }
 
