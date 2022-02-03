6120f1e4715ec72a63d11958df950b32e9ab4a7a
Your Name <you@example.com>
2022-02-03 18:11:17 -0300

    Cambios Clase 2

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,16 @@
 /*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+
+/* Esteban Camejo Ejercicio 3 EntradaSalida*/
+
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado;
+
+	nombreIngresado= document.getElementById("txtIdNombre").value;
+
+	alert(nombreIngresado);
+
 
 }
-
-
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,8 +2,16 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+/* Esteban Camejo Ejercicio 4 EntradaSalida*/
+
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	nombre = prompt("ingresar nombre")
+
+ document.getElementById("txtIdNombre").value = nombre;
+
+	alert(nombre);
 }
 
